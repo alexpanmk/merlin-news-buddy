228bbbe24e10852771090fa61a176b2f804cd0ef
alexpanmk <alexpanmk@gmail.com>
2023-11-14 09:35:49 +0800

    feat: stub MainNavigation component

---
+++ src/Navigation/MainNavigation.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import { Avatar, Button, Box, Nav, Stack, Text } from "grommet";
+
+import {
+  Analytics,
+  Chat,
+  Clock,
+  Configure,
+  Help,
+  Projects,
+  Split,
+  StatusInfoSmall,
+} from "grommet-icons";
+
+const MainNavigation = () => {
+  return <div>MainNavigation</div>;
+};
+
+export default MainNavigation;
