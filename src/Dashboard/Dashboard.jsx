44df5bfa86a6db86e564a543a0e168b08d6f1824
alexpanmk <alexpanmk@gmail.com>
2023-11-14 19:39:49 +0800

    refactor: refactor Dashboard and Searchbar

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import { TextInput } from "grommet";
 import NewsList from "../News/NewsList";
 import SearchBar from "./SearchBar";
+import { Box } from "grommet";
 
 const Dashboard = () => {
   return (
     <>
-      <SearchBar />
-      <NewsList />
+      <Box gridArea="main" pad="small">
+        <SearchBar />
+        <NewsList />
+      </Box>
     </>
   );
 };
--- src/Dashboard/SearchBar.jsx
+++ src/Dashboard/SearchBar.jsx
@@ -1,22 +1,33 @@
-import React from "react";
+import { useState, React } from "react";
 import { Box, TextInput } from "grommet";
 import NewsList from "../News/NewsList";
 import UseStore from "../UseStore";
 
 const SearchBar = () => {
+  //For the search bar
+  const [searchInput, setSearchInput] = useState("");
+
+  //For the search keyword to be fetched
   const [search, setSearch] = UseStore((state) => [
     state.search,
     state.setSearch,
   ]);
+
   return (
     <>
-      <Box gridArea="main" pad="small">
-        <TextInput
-          placeholder="Search News..."
-          style={{ background: "white", marginBottom: "20px" }}
-        />
-        <NewsList />
-      </Box>
+      <TextInput
+        value={searchInput}
+        onChange={(e) => {
+          setSearchInput(e.target.value);
+        }}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            setSearch(searchInput);
+          }
+        }}
+        placeholder="Search News..."
+        style={{ background: "white", marginBottom: "20px" }}
+      />
     </>
   );
 };
