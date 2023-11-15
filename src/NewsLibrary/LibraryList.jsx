7df4cf1537059d9a264dfaf04ebb0fff99a5eb5a
alexpanmk <alexpanmk@gmail.com>
2023-11-15 17:38:38 +0800

    feat: update LibraryList component

--- src/NewsLibrary/LibraryList.jsx
+++ src/NewsLibrary/LibraryList.jsx
@@ -5,6 +5,8 @@ import UseStore from "../UseStore";
 //To bring in savedNews from store and all corresponding methods
 
 const LibraryList = () => {
+  //   const { animate, multiple, ...rest } = props;
+
   const [savedNews, setSavedNews] = UseStore((state) => [
     state.savedNews,
     state.setSavedNews,
@@ -15,7 +17,30 @@ const LibraryList = () => {
     setSavedNews();
   }, []);
 
-  return <div>LibraryList</div>;
+  return (
+    <Box>
+      <Accordion>
+        {/* <Accordion animate={animate} multiple={multiple}> */}
+        <AccordionPanel label="Panel 1">
+          <Box background="light-2" overflow="auto" height="medium">
+            <Box height="large" flex={false}>
+              Panel 1 contents
+            </Box>
+          </Box>
+        </AccordionPanel>
+        <AccordionPanel label="Panel 2">
+          <Box background="light-2" style={{ height: "50px" }}>
+            Panel 2 contents
+          </Box>
+        </AccordionPanel>
+        <AccordionPanel label="Panel 3">
+          <Box background="light-2" style={{ height: "300px" }}>
+            Panel 3 contents
+          </Box>
+        </AccordionPanel>
+      </Accordion>
+    </Box>
+  );
 };
 
 export default LibraryList;
