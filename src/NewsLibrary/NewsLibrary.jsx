53c6c500bf6aeb339f222508ae8e4d79a4321ac6
alexpanmk <alexpanmk@gmail.com>
2023-11-17 10:57:02 +0800

    feat: stub SavedNewsAccordionPanelContent component

--- src/NewsLibrary/NewsLibrary.jsx
+++ src/NewsLibrary/NewsLibrary.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Box } from "grommet";
+import SavedNewsAccordion from "./SavedNewsItem/SavedNewsAccordion";
 
 const NewsLibrary = () => {
   return (
     <>
-      <Box gridArea="main" pad="small"></Box>
+      <Box gridArea="main" pad="small">
+        <SavedNewsAccordion />
+      </Box>
     </>
   );
 };
---
+++ src/NewsLibrary/SavedNewsItem/SavedNewsAccordionPanelContent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SavedNewsAccordionPanelContent = () => {
+  return <div>SavedNewsAccordionPanelContent</div>;
+};
+
+export default SavedNewsAccordionPanelContent;
