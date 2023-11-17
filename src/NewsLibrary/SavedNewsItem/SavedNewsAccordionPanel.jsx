e46be2b7b3adf55c6a52dd7e63dd97f1708dc3d4
alexpanmk <alexpanmk@gmail.com>
2023-11-17 16:43:48 +0800

    feat: update SavedNewsAccordion component

--- src/NewsLibrary/SavedNewsItem/SavedNewsAccordionPanel.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const SavedNewsAccordionPanel = () => {
-  return <div>SavedNewsAccordionPanel</div>;
-};
-
-export default SavedNewsAccordionPanel;
--- src/NewsLibrary/SavedNewsItem/SavedNewsAccordionPanelContent.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const SavedNewsAccordionPanelContent = () => {
-  return <div>SavedNewsAccordionPanelContent</div>;
-};
-
-export default SavedNewsAccordionPanelContent;
