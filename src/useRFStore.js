87877b555f2ec8faaab18c93dcc19e241a0a6b20
alexpanmk <alexpanmk@gmail.com>
2023-11-21 16:24:59 +0800

    feat: complete useRFStore

--- src/useRFStore.js
+++ src/useRFStore.js
@@ -1,1 +1,34 @@
-import { create } from "zustand";
+import { create } from 'zustand';
+import {
+    Connection,
+    EdgeChange,
+    NodeChange,
+    addEdge,
+    applyNodeChanges,
+    applyEdgeChanges,
+} from 'reactflow';
+
+import initialNodes from './nodes';
+import initialEdges from './edges';
+
+const useStore = create((set, get) => ({
+    nodes: initialNodes,
+    edges: initialEdges,
+    onNodesChange: (changes) => {
+        set({
+            nodes: applyNodeChanges(changes, get().nodes),
+        });
+    },
+    onEdgesChange: (changes) => {
+        set({
+            edges: applyEdgeChanges(changes, get().edges),
+        });
+    },
+    onConnect: (connection) => {
+        set({
+            edges: addEdge(connection, get().edges),
+        });
+    },
+}));
+
+export default useStore;
