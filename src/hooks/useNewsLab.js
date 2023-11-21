afef73e54ec766efa439f01d9d4917462fef63b0
alexpanmk <alexpanmk@gmail.com>
2023-11-21 14:18:48 +0800

    feat: update useNewsLab hook

--- src/hooks/useNewsLab.js
+++ src/hooks/useNewsLab.js
@@ -1,3 +1,18 @@
 //hooks for NewsLab
 import { useState, useEffect  } from "react";
+import { useLocalStorage } from "@uidotdev/usehooks";
 
+const useNewsLab = () => {
+    const [nodes, setNodes] = useLocalStorage("nodes", []);
+    const [newsLabInitialLoad, setNewsLabInitialLoad] = useState(false);
+    //add node method
+    const addNode = (node) => {
+        setNodes([...nodes, node]);
+    }
+    //delete node method
+    const deleteNode = (node) => {
+        setNodes(nodes.filter((n) => n.id !== node.id));
+    }
+}
+
+export default useNewsLab;
