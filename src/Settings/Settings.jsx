215fe266433f437549e07de5165584a70fc4b43b
alexpanmk <alexpanmk@gmail.com>
2023-11-23 09:45:35 +0800

    docs:

--- src/Settings/Settings.jsx
+++ src/Settings/Settings.jsx
@@ -1,3 +1,8 @@
+//Settings for API keys and theme modes (dark/light)
+//TODO: OpenAI API key / To fall back on normal search and disable chat if OpenAI API key is not available.
+//TODO: Air Table Bearer Token and Table Name
+//TODO: Theme mode switcher.
+
 import React from "react";
 
 const Settings = () => {
